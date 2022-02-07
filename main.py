7f757074dbe6a03564f727db68fc4726f5f9569d
Balogh Vince <baloghvince.bv@gmail.com>
2022-02-07 10:32:27 +0100

    Beolvasás, feladat, input

---
+++ main.py
@@ -0,0 +1,16 @@
+class Település:
+    lista = []
+    def __init__(self, azonosito, nev, rang, kistbes, területe, népessége, lakásszám):
+        self.azonosito = azonosito
+        self.nev = nev
+        self.rang = rang
+        self.kistbes = kistbes
+        self.területe = területe
+        self.népessége = népessége
+        self.lakásszám = lakásszám
+        Település.lista.append(self)
+
+with open("input.txt", "r", encoding="utf8") as f:
+    for sor in f:
+        s = sor.strip().split("\t")
+        t = Település(int(s[0]), s[1], s[2], s[3], int(s[4]), int(s[5]), int(s[6]))
