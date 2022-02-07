04e2852e37d88f136d9c3ee65be95058f722afde
Balogh Vince <baloghvince.bv@gmail.com>
2022-02-07 11:06:47 +0100

    19-24. feladat kész

--- Vince.py
+++ Vince.py
@@ -37,27 +37,58 @@ def feladat_17(lista):
 
 # 19)	Melyik a Makói kistérség legkisebb területű települése(i)?
 
+def KistéerségLegnéptelenebbTelepülése(lista, kistérség):
+    temp = ""
+    for i in lista:
+        if i.kistbes == kistérség:
+            if temp == "" or i.népesség < temp.népesség:
+                temp = i
+    temp2 = []
+    for i in lista:
+        if i.kistbes == kistérség and i.népesség == temp.népesség:
+            temp2.append(i.nev)
+
+def feladat_19(lista):
+    return KistéerségLegnéptelenebbTelepülése(lista, "Makói")
 
 
 # 20)	Melyik a Szegedi kistérség legkisebb területű települése(i)?
 
+def feladat_20(lista):
+    return KistéerségLegnéptelenebbTelepülése(lista, "Szegedi")
 
 
 # 21)	Melyik a Szentesi kistérség legkisebb területű települése(i)?
 
+def feladat_21(lista):
+    return KistéerségLegnéptelenebbTelepülése(lista, "Szentesi")
 
 
 # 22)	Melyik a Makói kistérség legnépesebb települése(i)?
 
+def KistéerségLegnépesebbTelepülése(lista, kistérség):
+    temp = ""
+    for i in lista:
+        if i.kistbes == kistérség:
+            if temp == "" or i.népesség > temp.népesség:
+                temp = i
+    temp2 = []
+    for i in lista:
+        if i.kistbes == kistérség and i.népesség == temp.népesség:
+            temp2.append(i.nev)
 
+def feladat_22(lista):
+    return KistéerségLegnépesebbTelepülése(lista, "Makói")
 
 # 23)	Melyik a Szegedi kistérség legnépesebb települése(i)?
 
-
+def feladat_23(lista):
+    return KistéerségLegnépesebbTelepülése(lista, "Szegedi")
 
 # 24)	Melyik a Szentesi kistérség legnépesebb települése(i)?
 
-
+def feladat_24(lista):
+    return KistéerségLegnépesebbTelepülése(lista, "Szentesi")
 
 # 25)	Írja ki a Makói kistérség településeinek népsűrűségét!
 
